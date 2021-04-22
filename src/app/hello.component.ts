60b8dad8aef53f13f0242225ee5c292fb541ef65
sajedgit <sajedaiub@gmail.com>
2021-04-22 16:55:33 +0600

    3 files modified, 1 added

--- src/app/hello.component.ts
+++ src/app/hello.component.ts
@@ -1,10 +1,44 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input } from "@angular/core";
+import { FormControl, FormGroup, Validators } from "@angular/forms";
 
 @Component({
-  selector: 'hello',
-  template: `<h1>Hello {{name}}!</h1>`,
-  styles: [`h1 { font-family: Lato; }`]
+  selector: "hello",
+  templateUrl: "./hello.component.html",
+  //styleUrls: [ './app.component.css' ]
+  styles: [
+    `
+      h1 {
+        font-family: Lato;
+      }
+    `
+  ]
 })
-export class HelloComponent  {
-  @Input() name: string;
+export class HelloComponent {
+  // @Input() name: string;
+  // name = new FormControl("");
+
+  profile = new FormGroup({
+    name: new FormControl("", Validators.required),
+    age: new FormControl(""),
+    address: new FormGroup({
+      street: new FormControl(""),
+      city: new FormControl(""),
+      country: new FormControl("")
+    })
+  });
+
+  submit() {
+    console.warn(this.profile.value);
+  }
+  reset() {
+    this.profile.setValue({
+      name: "",
+      age: "",
+      address: {
+        street: "",
+        city: "",
+        country: ""
+      }
+    });
+  }
 }
